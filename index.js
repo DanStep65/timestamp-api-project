ef48a4da08d058be6be9d5b93ec1068154fbb89e
Daniel Stephen <73526667+DanStep65@users.noreply.github.com>
2022-06-05 11:03:22 +0000

    Finished the project by adding "Invalid Date" handler, empty date handler, and any date_string handler.

--- index.js
+++ index.js
@@ -27,55 +27,61 @@ app.get("/api/hello", function (req, res) {
 
 //route the 'url/api/<any date format>' to either get or post (just in case, even though the project only requires get method)
 app.route("/api/:date")
-  //route through the get method )
+  //route through the get method 
   .get((req, res) => {
     //differentiate the type of date_string that is included in the parameter by using '-'
-    if(req.params.date.match('-')){
+  
+
+      if(req.params.date.match('-')){
       //directly put in the parameter as the argument in the constructor
       const d = new Date(req.params.date);
       //do output in json, with Date.getTime() for unix format and Date.toUTCString() for utc format
-      res.json(
-        {
-          "unix": d.getTime(),
-          "utc": d.toUTCString()
-        });
+        if(!d.getTime() || !d.toUTCString()){
+          res.json({
+            "error": d.toUTCString()
+          });
+        }
+        else{
+          res.json(
+            {
+              "unix": d.getTime(),
+              "utc": d.toUTCString()
+            });          
+        }
     }
-    else{
-      //convert the date parameter to Number first before putting the parameter as an argument
-      const d = new Date(parseInt(req.params.date));
-      //do output in json, with Date.getTime() for unix format and Date.toUTCString() for utc format
-      res.json(
-        {
-          "unix": d.getTime(),
-          "utc": d.toUTCString()
-        });
-    }
-  })
-  //route through the post method
-  .post((req, res) => {
-    //differentiate the type of date_string that is included in the parameter by using '-'
-    if(req.params.date.match('-')){
-      //directly put in the parameter as the argument in the constructor
-      const d = new Date(req.params.date);
-      //do output in json, with Date.getTime() for unix format and Date.toUTCString() for utc format
-      res.json(
-        {
-          "unix": d.getTime(),
-          "utc": d.toUTCString()
-        });
-    }
-    else{
-      //convert the date parameter to Number first before putting the parameter as an argument
-      const d = new Date(parseInt(req.params.date));
-      //do output in json, with Date.getTime() for unix format and Date.toUTCString() for utc format
-      res.json(
-        {
-          "unix": d.getTime(),
-          "utc": d.toUTCString()
-        });
-    }
+      else{
+        let d;
+        if(req.params.date.match(/[A-Za-z]\w/g)){
+          d = new Date(req.params.date);
+        }
+        else{
+          //convert the date parameter to Number first before putting the parameter as an argument
+          d = new Date(parseInt(req.params.date));
+        }
+        //do output in json, with Date.getTime() for unix format and Date.toUTCString() for utc format
+        if(!d.getTime() || !d.toUTCString()){
+          res.json({
+            "error": d.toUTCString()
+          });
+        }
+        else{
+          res.json(
+            {
+              "unix": d.getTime(),
+              "utc": d.toUTCString()
+            });          
+        }
+      }
   });
 
+app.get("/api", (req,res) =>{
+      const d = new Date();
+      res.json(
+        {
+          "unix": d.getTime(),
+          "utc": d.toUTCString()
+        });
+});
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
